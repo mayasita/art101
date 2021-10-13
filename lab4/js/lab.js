c0f19a2cb8c7c9942fed6e74c774e3786609aa16
mayasita <maicoron@ucsc.edu>
2021-10-13 11:44:49 -0700

    javascript lab

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// Roshambo Computer Game.
+// Allows players to participate in a virtual application of the game rock, paper, scissors, complete with animated hand gestures and the ability to choose your play.
+// @link   mayasita.github.io/art101/lab4
+// @file   lab.js
+// @author MayaCorona
+// @since  10.13.21
+
+// Create hand sprites and animations
+// Place one hand on each side of the screen
+// Allow for user input (rock, paper, or scissors)
+// Display appropriate hand sprite
+// Calculate victor
+// Display results
+// Reset hand sprites for additional rounds
