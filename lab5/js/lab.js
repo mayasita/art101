e01ef053fb5217e1b11665173984a209da39490d
mayasita <maicoron@ucsc.edu>
2021-10-15 21:44:43 -0700

    lab 5 update

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+ *Author: Maya Corona <maicoron@ucsc.edu>
+ *Created: 15 October
+ *License: Public Domain
+ */
+
+// Define Variables
+var make = "Toyota";
+var model = "Corolla";
+var color = "blue-grey";
+var year = 2017;
+
+// calculate
+var age = 2021 - year;
+
+// output
+document.writeln("Make: " + make + "<br>") ;
+document.writeln("Model: " + model + "<br>") ;
+document.writeln("Color: " + color + "<br>") ;
+document.writeln("Year: " + year + "<br>") ;
+document.writeln("Age: " + year + " years<br>") ;
