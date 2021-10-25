321f7de2a3318eb90f2cbf3da2596a8269c5a9a2
mayasita <maicoron@ucsc.edu>
2021-10-25 13:18:16 -0700

    lab 7 update

---
+++ lab7/js/lab.js
@@ -0,0 +1,25 @@
+/**
+ * Author: Maya Corona
+ * Created: October 25, 2021
+ *
+ **/
+
+// sortUserName - function that will take user unput to arrange and output their sortUserName
+function sortUserName() {
+   var userName = window.prompt("Hi. Please tell me your name so I can fix it.");
+   console.log("userName =" + userName);
+   // split string into an array
+   var nameArray = userName.split('');
+   console.log("nameArraySort =", nameArray);
+   // sort array
+   var nameArraySort = nameArray.sort();
+   console.log("nameArraySort =", nameArraySort);
+   // join array back into a string
+   var nameSorted = nameArraySort.join('');
+   console.log("nameSorted =", nameSorted);
+   return nameSorted;
+}
+
+// Output
+document.writeln("Hello! I've fixed your name: ",
+    sortUserName(), "</br>");
