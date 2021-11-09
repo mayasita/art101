eb6d5596b75d89a9a5ed881e647e19df7ab4d8a4
mayasita <maicoron@ucsc.edu>
2021-11-08 21:41:07 -0800

    lab 11

---
+++ lab11/js/lab.js
@@ -0,0 +1,24 @@
+/**
+ *
+ * Author: Maya Corona
+ * Created: November 8, 2021
+ *
+ **/
+
+//add event listener to button
+$("button.snap").click(function(){
+//toggle class special within div of section
+  $("challenge").toggleClass("beep");
+});
+
+//add event listener to button
+$("button.crackle").click(function(){
+//toggle class special within div of section
+  $("#problem").toggleClass("boop");
+});
+
+//add event listener to button
+$("button.pop").click(function(){
+//toggle class special within div of section
+  $("result").toggleClass("bop");
+});
